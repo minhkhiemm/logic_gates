a9e97105552ed613fb0ecd1b4771024c036ecd19
minhkhiemm <vominhkhiem212@gmail.com>
2021-01-09 10:52:28 +0700

    Add Travis CI

---
+++ target/debug/deps/half_adder-753a837daf2f362e.d
@@ -0,0 +1,5 @@
+/Users/vokhiem/rust/logic_gates/logic_gates/target/debug/deps/half_adder-753a837daf2f362e.rmeta: tests/half_adder.rs
+
+/Users/vokhiem/rust/logic_gates/logic_gates/target/debug/deps/half_adder-753a837daf2f362e.d: tests/half_adder.rs
+
+tests/half_adder.rs:
---
+++ target/debug/deps/logic_gates-f1efe3239358e4b6.d
@@ -0,0 +1,5 @@
+/Users/vokhiem/rust/logic_gates/logic_gates/target/debug/deps/logic_gates-f1efe3239358e4b6.rmeta: src/lib.rs
+
+/Users/vokhiem/rust/logic_gates/logic_gates/target/debug/deps/logic_gates-f1efe3239358e4b6.d: src/lib.rs
+
+src/lib.rs:
